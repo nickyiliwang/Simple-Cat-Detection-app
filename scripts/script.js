274dab5b5dfa58059abffdc12212ae41754d7905
Nick Wang <nickyiliwang@gmail.com>
2019-11-07 15:42:41 -0500

    with test images

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,12 @@
 // selecting html elements
 const img = document.getElementById("img");
 const cat = document.querySelector(".show-cats");
+const testImg = document.querySelectorAll(".test-img");
 
 // "myAwesomeDropzone" is the camelized version of the HTML element's ID
 Dropzone.options.myAwesomeDropzone = {
   paramName: "file", // The name that will be used to transfer the file
-  accept: function(file, url, done) {
+  accept: function(file, done) {
     console.log(file);
     // console.log(file.previewElement);
     console.log(file.previewTemplate);
@@ -27,3 +28,22 @@ Dropzone.options.myAwesomeDropzone = {
     // Tensor
   }
 };
+
+testImg.forEach(listener => {
+  listener.addEventListener("click", e => {
+    img.src = e.target.src;
+
+    // Tensor
+    // Load the model.
+    mobilenet.load().then(model => {
+      console.log(model);
+      // Classify the image.
+      model.classify(img).then(predictions => {
+        let predictionPercent = Math.floor(predictions[0].probability * 100);
+
+        cat.textContent = `Is this a ${predictions[0].className} cat ? I am ${predictionPercent}% sure about this.`;
+      });
+    });
+    // Tensor
+  });
+});
