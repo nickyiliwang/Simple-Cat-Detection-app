79c9ebda8d169670ca2cb0edbef832c2ddf91d21
Nick Wang <nickyiliwang@gmail.com>
2019-11-07 16:12:27 -0500

    url submit works

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,8 @@
 const img = document.getElementById("img");
 const cat = document.querySelector(".show-cats");
 const testImg = document.querySelectorAll(".test-img");
+const urlUpload = document.querySelector(".url-upload");
+const urlInput = document.querySelector(".url-input");
 
 // "myAwesomeDropzone" is the camelized version of the HTML element's ID
 Dropzone.options.myAwesomeDropzone = {
@@ -12,20 +14,7 @@ Dropzone.options.myAwesomeDropzone = {
     console.log(file.previewTemplate);
     img.src = file.upload.filename;
 
-    // Tensor
-    // Load the model.
-    mobilenet.load().then(model => {
-      console.log(model);
-      // Classify the image.
-      model.classify(img).then(predictions => {
-        let predictionPercent = Math.floor(predictions[0].probability * 100);
-
-        cat.textContent = `Is this a ${predictions[0].className} cat ? I am ${predictionPercent}% sure about this.`;
-
-        console.log(predictions);
-      });
-    });
-    // Tensor
+    tensorFlow();
   }
 };
 
@@ -33,17 +22,29 @@ testImg.forEach(listener => {
   listener.addEventListener("click", e => {
     img.src = e.target.src;
 
-    // Tensor
-    // Load the model.
-    mobilenet.load().then(model => {
-      console.log(model);
-      // Classify the image.
-      model.classify(img).then(predictions => {
-        let predictionPercent = Math.floor(predictions[0].probability * 100);
-
-        cat.textContent = `Is this a ${predictions[0].className} cat ? I am ${predictionPercent}% sure about this.`;
-      });
-    });
-    // Tensor
+    tensorFlow();
   });
 });
+
+urlUpload.addEventListener("submit", e => {
+  e.preventDefault();
+
+  img.src = urlInput.value;
+  tensorFlow();
+});
+
+// Tensor
+// this function just runs the classification function
+const tensorFlow = () => {
+  // Load the model.
+  mobilenet.load().then(model => {
+    console.log(model);
+    // Classify the image.
+    model.classify(img).then(predictions => {
+      let predictionPercent = Math.floor(predictions[0].probability * 100);
+
+      cat.textContent = `Is this a ${predictions[0].className} cat ? I am ${predictionPercent}% sure about this.`;
+    });
+  });
+};
+// Tensor
